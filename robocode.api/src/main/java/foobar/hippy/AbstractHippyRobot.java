41a1e73e5caece9e559de85f117bfd5506745dff
pablucu <pablucu@noooooooooooooooomail.com>
2014-11-25 16:17:21 +0000

    Comments on AbstractHippyRobot

--- robocode.api/src/main/java/foobar/hippy/AbstractHippyRobot.java
+++ robocode.api/src/main/java/foobar/hippy/AbstractHippyRobot.java
@@ -2,35 +2,79 @@ package foobar.hippy;
 
 import robocode.Robot;
 
+/**
+ * Abstract class that identifies the Robot class based on the name.
+ * 
+ * @author rodriguezp
+ */
 abstract public class AbstractHippyRobot extends Robot {
 
-	private static final String STONE = "Stone";
+	/**
+	 * String that defines if a Robot is an Animal.
+	 */
 	private static final String ANIMAL = "Animal";
+
+	/**
+	 * String that defines if a Robot is a Stone.
+	 */
+	private static final String STONE = "Stone";
+
+	/**
+	 * String that defines if a Robot is a Treasure.
+	 */
 	private static final String TREASURE = "Treasure";
 
-	public static boolean isVan(String s) {
-		return !isStone(s) && !isAnimal(s) && !isTreasure(s);
-	}
-
-	public static boolean isStone(String s) {
-		if (s == null || s.isEmpty()) {
+	/**
+	 * Checks if a Robot is an Animal using its name.
+	 * 
+	 * @param name
+	 *            name of the Robot
+	 * @return true if the Robot is an Animal, false otherwise.
+	 */
+	public static boolean isAnimal(String name) {
+		if (name == null || name.isEmpty()) {
 			return false;
 		}
-		return s.toLowerCase().contains(STONE.toLowerCase());
+		return name.toLowerCase().contains(ANIMAL.toLowerCase());
 	}
 
-	public static boolean isAnimal(String s) {
-		if (s == null || s.isEmpty()) {
+	/**
+	 * Checks if a Robot is a Stone using its name.
+	 * 
+	 * @param name
+	 *            name of the Robot
+	 * @return true if the Robot is a Stone, false otherwise.
+	 */
+	public static boolean isStone(String name) {
+		if (name == null || name.isEmpty()) {
 			return false;
 		}
-		return s.toLowerCase().contains(ANIMAL.toLowerCase());
+		return name.toLowerCase().contains(STONE.toLowerCase());
 	}
 
-	public static boolean isTreasure(String s) {
-		if (s == null || s.isEmpty()) {
+	/**
+	 * Checks if a Robot is a Treasure using its name.
+	 * 
+	 * @param name
+	 *            name of the Robot
+	 * @return true if the Robot is a Treasure, false otherwise.
+	 */
+	public static boolean isTreasure(String name) {
+		if (name == null || name.isEmpty()) {
 			return false;
 		}
-		return s.toLowerCase().contains(TREASURE.toLowerCase());
+		return name.toLowerCase().contains(TREASURE.toLowerCase());
+	}
+
+	/**
+	 * Checks if a Robot is a Van using its name.
+	 * 
+	 * @param name
+	 *            name of the Robot
+	 * @return true if the Robot is a Van, false otherwise.
+	 */
+	public static boolean isVan(String name) {
+		return !isStone(name) && !isAnimal(name) && !isTreasure(name);
 	}
 
 }
